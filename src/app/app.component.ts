6a919d7191426c9661b6e520d5d4d2c0f9755c0d
vilayan <valuator27@gmail.com>
2017-06-23 21:31:57 +0700

    Setup Components and Modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <md-sidenav-container fullscreen>
+
+      <md-sidenav #sidenav mode="over">
+        Jolly good!
+      </md-sidenav>
+      <md-toolbar color="primary">
+        <button type="button" md-button (click)="sidenav.open()">
+          <md-icon>dashboard</md-icon>
+        </button>
+      </md-toolbar>
+
+    </md-sidenav-container>
+  `,
+  styles: [`
+    md-sidenav {
+      width: 200px;
+    }
+  `]
 })
 export class AppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,50 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+/***Angular Libary*/
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MdButtonModule, MdCardModule, MdDialogModule, MdGridListModule, MdIconModule, MdIconRegistry, MdInputModule,
+  MdMenuModule,
+  MdProgressSpinnerModule,
+  MdSidenavModule,
+  MdToolbarModule, MdTooltipModule
+} from '@angular/material';
+import 'hammerjs';
+import {PushNotificationsModule, SimpleNotificationsModule} from 'angular2-notifications/dist';
+
+/***Angular Component*/
+import { MapComponent } from './map/map.component';
+import { WeatherComponent } from './weather/weather.component';
+import { HistoryComponent } from './history/history.component';
+import { HisdetailComponent } from './history/hisdetail/hisdetail.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    WeatherComponent,
+    HistoryComponent,
+    HisdetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdToolbarModule,
+    MdMenuModule,
+    MdCardModule,
+    MdInputModule,
+    MdIconModule,
+    MdProgressSpinnerModule,
+    MdTooltipModule,
+    MdGridListModule,
+    MdSidenavModule,
+    MdDialogModule,
+    PushNotificationsModule,
+    SimpleNotificationsModule.forRoot()
   ],
-  providers: [],
+  providers: [MdIconRegistry],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
