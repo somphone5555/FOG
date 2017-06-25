8ed52a74753f83088dfc2e077c224145100ea0f2
vilayan <valuator27@gmail.com>
2017-06-25 14:08:09 +0700

    edit History component

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -7,27 +7,51 @@ import {Component, OnInit} from '@angular/core';
     <div fxFlex.lg="20" fxFlex.sm="0" fxFlex.xs="0"></div>
     <div fxLayout="column" fxFlex.lg="60" fxFlex.sm="100" fxFlex.xs="100">
       <md-card>
+        <button md-mini-fab class="removebtn"><md-icon>remove</md-icon></button>
+        <md-card-title style="font-size: 42px">La</md-card-title>
+        <md-card-subtitle style="font-size: 22px">Vientaine ssdfsfd sdfsdfs</md-card-subtitle>
         <div fxLayout="row" fxLayoutAlign="space-between stretch">
           <div fxFlex="50" fxLayout="column">
             
-            <md-card-title style="font-size: 42px">La</md-card-title>
-            <md-card-subtitle style="font-size: 22px">Vientaine</md-card-subtitle>
+            
             <md-card-title>Now</md-card-title>
             <span><md-icon style="font-size: 50px;">cloud</md-icon></span>
             <span style="font-size: 50px;">30ໍ </span>
             <p>Cloud <br>Boken cloud</p>
 
           </div>
-          <div fxFlex="50" fxLayout="column" style="margin-top: 4rem">
-            <button md-fab class="removebtn"><md-icon>remove</md-icon></button>
+          <div fxFlex="50" fxLayout="column">
             <md-card-title>History</md-card-title>
+            <br>
             <span><md-icon>cloud</md-icon></span>
             <span style="font-size: 50px;">30ໍ </span>
             <p>Cloud <br>Boken cloud</p>
           </div>
-          <div fxFlex="10">
-            
+          
+        </div>
+      </md-card>
+      <md-card>
+        <button md-mini-fab class="removebtn"><md-icon>remove</md-icon></button>
+        <md-card-title style="font-size: 42px">La</md-card-title>
+        <md-card-subtitle style="font-size: 22px">Vientaine ssdfsfd sdfsdfs</md-card-subtitle>
+        <div fxLayout="row" fxLayoutAlign="space-between stretch">
+          <div fxFlex="50" fxLayout="column">
+
+
+            <md-card-title>Now</md-card-title>
+            <span><md-icon style="font-size: 50px;">cloud</md-icon></span>
+            <span style="font-size: 50px;">30ໍ </span>
+            <p>Cloud <br>Boken cloud</p>
+
           </div>
+          <div fxFlex="50" fxLayout="column">
+            <md-card-title>History</md-card-title>
+            <br>
+            <span><md-icon>cloud</md-icon></span>
+            <span style="font-size: 50px;">30ໍ </span>
+            <p>Cloud <br>Boken cloud</p>
+          </div>
+
         </div>
       </md-card>
       
@@ -39,9 +63,10 @@ import {Component, OnInit} from '@angular/core';
 
       margin: 1rem;
     }
-    removebtn{
-      top: 5px;
-      right: 5px;
+    .removebtn {
+      position: absolute;
+      right: 10px;
+      top: 10px;
     }
   `]
 })
