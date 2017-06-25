32757b079ae1fd079eaefc8b69159a53a856a2c7
vilayan <valuator27@gmail.com>
2017-06-25 14:48:15 +0700

    add weather in map component

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,11 +7,47 @@ import {WmService} from '../Services/wm.service';
     <agm-map [latitude]="lat" [longitude]="lng" (mapClick)="newMarker($event)">
       <agm-marker [latitude]="lat" [longitude]="lng"></agm-marker>
     </agm-map>
+    <md-card>
+      <button md-mini-fab style="position: absolute;right: 5px;top:5px;"><md-icon>add</md-icon></button>
+      <div fxLayout="row" fxLayoutAlign="space-between none">
+        <div>
+          <md-card-title style="font-size: 25px">LA</md-card-title>
+          <md-card-subtitle style="font-size: 15px">Vientaine</md-card-subtitle>
+        </div>
+        <div>
+
+        </div>
+      </div>
+      <md-card-content class="weathercontent">
+        
+        <div fxLayout="row">
+          <span fxFlex="50" style="font-size: 50px;">30ໍ </span>
+          <span fxFlex="50">
+            <!--<img [src]='firstArea?.icon_url'
+                                 alt='Icon depicting current weather.' width="100">-->
+            <md-icon style="font-size: 40px">cloud</md-icon>
+          </span>
+          <md-card-subtitle>cloud</md-card-subtitle>
+        </div>
+        <div fxLayout="row" fxLayoutAlign="space-between center" style="font-size: 1em">
+          <div fxFlex="50" style="font-size: 1rem">
+
+          </div>
+        </div>
+        <strong>Wind : </strong><span>dmdkdkkdkd ldk dkf</span><br>
+      </md-card-content>
+    </md-card>
   `,
   styles: [`
     agm-map{
       width: 100%;
       height: 91%;
+    }
+    md-card{
+      position: absolute;
+      left: 5px;
+      bottom: 10px;
+      width: 180px;
     }
   `]
 })
